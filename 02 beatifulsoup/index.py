f3f6f1102428ed4e1d225f77c1b9897e3a20be06
Javier Trujillo <javier.trujillo.jimenez@gmail.com>
2022-08-11 20:32:07 +0200

    Termino el tutorial con Beatiful Soup, solo me salto las paginaciones, lo he visto en video pero no las implemento

--- 02 beatifulsoup/index.py
+++ 02 beatifulsoup/index.py
@@ -1,23 +1,30 @@
 from bs4 import BeautifulSoup
 import requests
 
-website = 'https://subslikescript.com/movie/Titanic-46435'
+root = 'https://subslikescript.com'
+website = f'{root}/movies'
 result = requests.get(website)
 content = result.text
-
-#soup = BeautifulSoup(content, 'lxml')
-soup = BeautifulSoup(content, 'html.parser')
+soup = BeautifulSoup(content, 'lxml')
+#soup = BeautifulSoup(content, 'html.parser')
 
 # print(soup.prettify())
-
-
 box = soup.find('article', class_='main-article')
 
-title = box.find('h1').get_text()
+links = []
+for link in box.find_all('a', href=True):
+    links.append(link['href'])
 
-transcript = box.find('div', class_='full-script').get_text(strip=True, separator=' ')
-print(title)
-print(transcript)
-
-with open(f'{title}.txt','w', encoding="utf-8") as file:
-    file.write(transcript)
+i=0
+for link in links:
+    website = f'{root}/{link}'
+    result = requests.get(website)
+    content = result.text
+    soup = BeautifulSoup(content, 'lxml')
+    box = soup.find('article', class_='main-article')
+    title = box.find('h1').get_text()
+    transcript = box.find('div', class_='full-script').get_text(strip=True, separator=' ')
+    with open(f'transcript{i}.txt','w', encoding="utf-8") as file:
+        file.write(transcript)
+    i=i+1
+    
