8202d23e2b93aae598dc2d974356acf71956bb7e
Javier Trujillo <javier.trujillo.jimenez@gmail.com>
2022-08-11 15:31:45 +0200

    generando mi primer archivo desde Beatiful Soup

--- 02 beatifulsoup/index.py
+++ 02 beatifulsoup/index.py
@@ -18,3 +18,6 @@ title = box.find('h1').get_text()
 transcript = box.find('div', class_='full-script').get_text(strip=True, separator=' ')
 print(title)
 print(transcript)
+
+with open(f'{title}.txt','w', encoding="utf-8") as file:
+    file.write(transcript)
