3ea7a7b687ddb6f408913688c3c0ac6117e4298f
Javier Trujillo <javier.trujillo.jimenez@gmail.com>
2022-08-15 18:18:49 +0200

    consigo cambiar los selects para que me elija la liga española y de la temporada 21/22, que haga una pausa para recoger los datos de las tablas lo pasamos a un Dataframe de Pandas y por último lo exportamos a un csv

--- 03 selenium/index.py
+++ 03 selenium/index.py
@@ -1,5 +1,10 @@
+from time import sleep
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import Select
+import time
+import pandas as pd
+
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 website= 'https://www.adamchoi.co.uk/teamgoals/detailed'
@@ -7,3 +12,22 @@ driver.get(website)
 
 all_matches_button = driver.find_element_by_xpath('//label[@analytics-event="All matches"]')
 all_matches_button.click()
+
+dropdown = Select(driver.find_element_by_id('country'))
+dropdown.select_by_visible_text('Spain')
+dropdown = Select(driver.find_element_by_id('season'))
+dropdown.select_by_visible_text('21/22')
+time.sleep(3)
+
+matches = driver.find_elements_by_tag_name('tr')
+
+partidos = []
+for match in matches:
+    #print(match.text)
+    partidos.append(match.text)
+
+driver.quit()
+
+df = pd.DataFrame({'partidos':partidos})
+print(df)
+df.to_csv('partidos.csv', index=False)
