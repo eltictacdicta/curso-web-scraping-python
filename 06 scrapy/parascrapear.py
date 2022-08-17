d142ddec60d28171f6f0b530ed1bde0d4287c599
Javier Trujillo <javier.trujillo.jimenez@gmail.com>
2022-08-17 20:19:43 +0200

    La primera practica que hago con scrapy es siguiendo el video de Nicolas Marin Torres donde hace un pequeño Crawler, aunque yo hago una pequeña modificación y en vez de pasarlo a Google Drive lo exporto en un csv local con pandas

---
+++ 06 scrapy/parascrapear.py
@@ -0,0 +1,25 @@
+import scrapy
+import pandas as pd
+
+frases_lista = []
+
+class ParascrapearSpider(scrapy.Spider):
+    name = 'parascrapear'
+    allowed_domains = ['parascrapear.com']
+    start_urls = ['http://parascrapear.com/']
+
+    def parse(self, response):
+        print('Parseando ' + response.url)       
+        
+        next_urls = response.css('a::attr(href)').getall()
+        for next_url in next_urls:
+            if next_url is not None:
+                yield scrapy.Request(response.urljoin(next_url))
+        
+        frases = response.css('q::text').getall()
+        for frase in frases:
+            if frase is not frases_lista:
+                frases_lista.append(frase)
+        df_frases = pd.DataFrame({"Frases":frases_lista})
+        df_frases.to_csv("frases.csv",index=False)
+    
